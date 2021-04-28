a0f7c805dccf07178412ab62eed82769eceeeb9e
Charles May <charlie@Charless-MacBook-Pro.local>
2021-04-28 10:23:41 -0500

    Client Profile bones created. Still needs state

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddClass from "./components/AddClass";
-import SignUp from './components/SignUp'
-import Login from './components/Login'
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import ClientProfile from "./components/ClientProfile";
 
 const initialFormValues = {
   className: "",
@@ -32,22 +33,24 @@ export default function App() {
       location: formValues.location,
       maxClassSize: formValues.maxClassSize.trim(),
     };
-    setClasses([...classes, newClass])
-    setFormValues(initialFormValues)
+    setClasses([...classes, newClass]);
+    setFormValues(initialFormValues);
   };
-
-
 
   return (
     <Router>
-    <div className="App">
-      <Switch>
-      {/* <AddClass values={formValues} submit={submitNewClass} update={updateForm} /> */}
-      <Route path='/signup' component={SignUp} />
-      <Route path='/login' component={Login} />
-      <Login />
-      </Switch>
-    </div>
+      <div className="App">
+        <Switch>
+          {/* <AddClass values={formValues} submit={submitNewClass} update={updateForm} /> */}
+          {/* <Route path="/class-list">
+          {classes.map((aClass) => {
+            return <Class details={aClass} />; */}
+          <Route path="/signup" component={SignUp} />
+          <Route path="/login" component={Login} />
+          <Login />
+        </Switch>
+        <ClientProfile />
+      </div>
     </Router>
   );
 }
